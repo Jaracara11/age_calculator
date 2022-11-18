41fd0c72ee5fd5ca6f63b227efc49f39e4498a8f
leduardo <leduardo@tredium.com>
2022-11-18 15:34:31 +0000

    get funcionando, pendiente configurar post endpoint

--- lib/age_calculator/application.ex
+++ lib/age_calculator/application.ex
@@ -8,8 +8,7 @@ defmodule AgeCalculator.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: AgeCalculator.Worker.start_link(arg)
-      # {AgeCalculator.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: RestApi.Router, options: [port: 8080]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
