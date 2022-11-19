1a9dd24969e16e20d5b26f6d96ddc133146809e4
Luis_Eduardo <undernet1@gmail.com>
2022-11-19 14:03:33 +0000

    se agregan config files

---
+++ config/test.env.exs
@@ -0,0 +1,3 @@
+import Config
+
+config :age_calculator, port: 8081
--- lib/age_calculator/application.ex
+++ lib/age_calculator/application.ex
@@ -1,8 +1,4 @@
 defmodule AgeCalculator.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   @impl true
@@ -11,8 +7,6 @@ defmodule AgeCalculator.Application do
       {Plug.Cowboy, scheme: :http, plug: RestApi.Router, options: [port: 8080]}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: AgeCalculator.Supervisor]
     Supervisor.start_link(children, opts)
   end
